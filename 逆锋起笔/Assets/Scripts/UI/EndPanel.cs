ecee4b1e4bee8f45875f05969cb91cbe653c7f8e
Qizi <531387937@qq.com>
2021-11-26 19:11:12 +0800

    endPanel

---
+++ 逆锋起笔/Assets/Scripts/UI/EndPanel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EndPanel : MonoBehaviour
+{
+    public Text winInfo;
+    public Text EndTimer;
+    float timer = 0;
+    bool gameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EndTimer.text = timer.ToString() + "s";
+        if(timer>0&&gameOver)
+        {
+            timer -= Time.deltaTime;
+        }
+        if(gameOver&&timer<=0)
+        {
+            UIManager.Instance.ContiuneGame(true);
+        }
+    }
+
+    public void GameOver(List<Player> players)
+    {
+        gameOver = true;
+        for(int i = 0;i<players.Count;i++)
+        {
+            winInfo.text = players[i].playerName + "和牌,牌型为" + players[i].wintype.ToString()+"\n";
+        }
+        timer = 10;
+    }
+
+    private void OnDisable()
+    {
+        gameOver = false;
+    }
+}
