23ea760854b5bed84a5a188e536fc6e79c5dbe20
Major <lmaj7@qq.com>
2021-11-28 00:33:22 +0800

    上传音乐

---
+++ 逆锋起笔/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MAINBGM = 1691765453U;
+        static const AkUniqueID SET_STATE_BRIDGE = 1396041131U;
+        static const AkUniqueID SET_STATE_CHORUS = 1603954450U;
+        static const AkUniqueID SET_STATE_NONE = 4160240018U;
+        static const AkUniqueID SET_STATE_NORMAL = 2630068541U;
+        static const AkUniqueID STOP_MAINBGM = 4093728171U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLAYING_STATE
+        {
+            static const AkUniqueID GROUP = 1431182499U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BRIDGE = 2068062714U;
+                static const AkUniqueID CHORUS = 2866621671U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID NORMAL = 1160234136U;
+            } // namespace STATE
+        } // namespace PLAYING_STATE
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID COMMON = 2395677314U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
